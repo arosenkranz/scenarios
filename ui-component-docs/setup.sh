075db5294e1678ed159f2a1ac8f466ac4ab590ef
Alex Rosenkranz <alex.rosenkranz@gmail.com>
2021-05-08 09:06:58 -0400

    adds starting path

--- ui-component-docs/setup.sh
+++ ui-component-docs/setup.sh
@@ -2,7 +2,6 @@
 
 git clone https://github.com/arosenkranz/component-library-demo
 cd component-library-demo
-ls
 # docker run --rm -it --mount type=bind,source="$(pwd)",target=/src -p 6006:6006 -u node  node:14.16.0-alpine3.13 npm install && npm run storybook --bind=0.0.0.0
 # ls
 npm install
