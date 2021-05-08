b6df25e33bcf6720d795f67663599b35ed203f73
Alex Rosenkranz <alex.rosenkranz@gmail.com>
2021-05-07 23:22:39 -0400

    setting up with vscode

--- ui-component-docs/setup.sh
+++ ui-component-docs/setup.sh
@@ -3,8 +3,8 @@
 git clone https://github.com/arosenkranz/component-library-demo
 cd component-library-demo
 ls
-docker run --rm -it --mount type=bind,source="$(pwd)",target=/src -p 6006:6006 -u node  node:14.16.0-alpine3.13 npm install && npm run storybook --bind=0.0.0.0
+# docker run --rm -it --mount type=bind,source="$(pwd)",target=/src -p 6006:6006 -u node  node:14.16.0-alpine3.13 npm install && npm run storybook --bind=0.0.0.0
 # ls
-# npm install
-# echo "Starting..."
-# npm run storybook
+npm install
+echo "Starting..."
+npm run storybook
