1f85b39c9cb1cbc97193e4083a3294ad5228361a
Alex Rosenkranz <alex.rosenkranz@gmail.com>
2021-05-08 11:20:36 -0400

    feat: updates first command

--- ui-component-docs/setup.sh
+++ ui-component-docs/setup.sh
@@ -21,8 +21,9 @@ VSCODEEOF
 
 git clone https://github.com/arosenkranz/component-library-demo
 cd component-library-demo
+git fetch && git checkout start
 # docker run --rm -it --mount type=bind,source="$(pwd)",target=/src -p 6006:6006 -u node  node:14.16.0-alpine3.13 npm install && npm run storybook --bind=0.0.0.0
 # ls
 npm install
 echo "Starting..."
-npm run storybook
+npm run dev
