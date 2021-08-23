6c050b06080987dc1bc46314376e5086880606e6
Alex Rosenkranz <alex.rosenkranz@datadoghq.com>
2021-08-23 14:33:29 -0400

    feat: adds docker compose script

--- notebook-api-lab/background.sh
+++ notebook-api-lab/background.sh
@@ -3,11 +3,12 @@
 curl -s https://datadoghq.dev/katacodalabtools/r?raw=true|bash
 
 mkdir /root/lab
-mv /root/docker-compose.yml /root/lab
 
 cd /ecommworkshop
 git fetch
 git checkout tags/2.0.0
+
+mv /ecommworkshop/deploy/docker-compose/docker-compose-fixed-instrumented.yml /root/lab/docker-compose.yml
 
 docker-compose -f /root/lab/docker-compose.yml pull
 
