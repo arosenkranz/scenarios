543a5e3952820c9b9c53a4f12daa6bcdf573bce4
Alex Rosenkranz <alex.rosenkranz@datadoghq.com>
2021-09-23 15:53:49 -0400

    updates env vars

--- workshop-dash-frontend-2021/01-getting-started/foreground.sh
+++ workshop-dash-frontend-2021/01-getting-started/foreground.sh
@@ -4,6 +4,8 @@ export POSTGRES_USER=postgres
 export POSTGRES_PASSWORD=postgres
 export DD_DISCOUNTS_URL=https://[[HOST_SUBDOMAIN]]-5001-[[KATACODA_HOST]].environments.katacoda.com/discount
 export DD_ADS_URL=https://[[HOST_SUBDOMAIN]]-5002-[[KATACODA_HOST]].environments.katacoda.com/
+export REACT_APP_DD_DISCOUNTS_URL=https://[[HOST_SUBDOMAIN]]-5001-[[KATACODA_HOST]].environments.katacoda.com/discount
+export REACT_APP_DD_ADS_URL=https://[[HOST_SUBDOMAIN]]-5002-[[KATACODA_HOST]].environments.katacoda.com/
 export RUM_START_URL=https://[[HOST_SUBDOMAIN]]-3000-[[KATACODA_HOST]].environments.katacoda.com/
 export MICROSITE_URL=https://[[HOST_SUBDOMAIN]]-3001-[[KATACODA_HOST]].environments.katacoda.com/
 
@@ -29,13 +31,6 @@ clear
 statuscheck environment
 
 cd /root/lab
-
-printf "DD_API_KEY=$DD_API_KEY\n\
-DD_APP_KEY=$DD_APP_KEY\n\
-DD_APPLICATION_ID=$DD_APPLICATION_ID\n\
-DD_CLIENT_TOKEN=$DD_CLIENT_TOKEN\n\
-POSTGRES_USER=postgres\n\
-POSTGRES_PASSWORD=postgres" > .env 
 
 statusupdate complete
 prepenvironment
