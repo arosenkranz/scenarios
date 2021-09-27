a7645d47f8bf89d9b254933868fd7e791cd533e7
Alex Rosenkranz <alex.rosenkranz@datadoghq.com>
2021-09-27 10:36:57 -0400

    updates to move files

--- dd101-developer/03-apm/background.sh
+++ dd101-developer/03-apm/background.sh
@@ -3,9 +3,9 @@
 curl -s https://datadoghq.dev/katacodalabtools/r?raw=true|bash
 
 mkdir /root/lab
-# mv /root/docker-compose.yml /root/lab
+mv /root/docker-compose.yml /root/lab
 
-# mv /root/discounts.py /ecommworkshop/discounts-service/discounts.py
+mv /root/discounts.py /ecommworkshop/discounts-service/discounts.py
 
 ln -s /ecommworkshop/store-frontend-broken-instrumented /root/lab/store-frontend
 ln -s /ecommworkshop/discounts-service /root/lab/discounts-service
