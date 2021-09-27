5aa75cd830e915afe0ea553300b00d1db74186ce
Alex Rosenkranz <alex.rosenkranz@datadoghq.com>
2021-09-27 09:57:14 -0400

    fixes mv command typo

--- dd101-developer/03-apm/background.sh
+++ dd101-developer/03-apm/background.sh
@@ -5,7 +5,7 @@ curl -s https://datadoghq.dev/katacodalabtools/r?raw=true|bash
 mkdir /root/lab
 mv /root/docker-compose.yml /root/lab
 
-mv /root/discounts.py /ecommworkshop/discounts-service
+mv /root/discounts.py /ecommworkshop/discounts-service/discounts.py
 
 ln -s /ecommworkshop/store-frontend-broken-instrumented /root/lab/store-frontend
 ln -s /ecommworkshop/discounts-service /root/lab/discounts-service
