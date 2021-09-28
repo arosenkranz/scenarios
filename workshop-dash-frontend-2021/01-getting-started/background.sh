72c60b8048d020c5dd3cb2a6154753f89b52af25
Alex Rosenkranz <alex.rosenkranz@datadoghq.com>
2021-09-28 16:40:57 -0400

    updates symlinks

--- workshop-dash-frontend-2021/01-getting-started/background.sh
+++ workshop-dash-frontend-2021/01-getting-started/background.sh
@@ -9,23 +9,20 @@ git clone https://github.com/arosenkranz/workshop-microsite.git /storedog-micros
 cd /storedog-microsite
 npm install --silent
 
-cd /ecommworkshop
-git fetch
-git checkout tags/2.0.0
-
 # Fetch discounts in the browser
 mv /root/home_controller.rb /ecommworkshop/store-frontend-instrumented-fixed/app/controllers/spree/home_controller.rb
 mv /root/spree_application.html.erb /ecommworkshop/store-frontend-instrumented-fixed/app/views/spree/layouts/spree_application.html.erb
 mv /root/index.html.erb /ecommworkshop/store-frontend-instrumented-fixed/app/views/spree/home/index.html.erb
-ln -s /ecommworkshop/store-frontend-instrumented-fixed/app/views/spree/layouts/spree_application.html.erb /root/lab/spree_application.html.erb
+
+ln -s /ecommworkshop/store-frontend-instrumented-fixed /root/lab/store-frontend
 
 mv /root/discounts-requirements.txt /ecommworkshop/discounts-service/requirements.txt
 mv /root/discounts.py /ecommworkshop/discounts-service/discounts.py
-ln -s /ecommworkshop/discounts-service/discounts.py /root/lab/discounts.py
+ln -s /ecommworkshop/discounts-service /root/lab/discounts-service
 
 mv /root/ads-requirements.txt /ecommworkshop/ads-service/requirements.txt
 mv /root/ads.py /ecommworkshop/ads-service/ads.py
-ln -s /ecommworkshop/ads-service/ads.py /root/lab/ads.py
+ln -s /ecommworkshop/ads-service /root/lab/ads-service
 
 # Link to the modified frontend configuration file
 mv /root/frontend-config.rb /ecommworkshop/store-frontend-instrumented-fixed/config/environments/development.rb
@@ -34,7 +31,8 @@ ln -s /ecommworkshop/store-frontend-instrumented-fixed/config/environments/devel
 # Link to microsite
 ln -s /storedog-microsite /root/lab/microsite
 
-docker pull node:alpine
+docker pull node:alpine 
+docker pull nginx 
 
 docker-compose -f /root/lab/docker-compose.yml pull
 
