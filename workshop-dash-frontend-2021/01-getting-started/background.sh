5485535e4e04a83e844f26f2ab299a3aa7542ba4
Alex Rosenkranz <alex.rosenkranz@datadoghq.com>
2021-09-28 20:11:22 -0400

    adds craco install

--- workshop-dash-frontend-2021/01-getting-started/background.sh
+++ workshop-dash-frontend-2021/01-getting-started/background.sh
@@ -8,7 +8,8 @@ mv /root/docker-compose-dev.yml /root/lab
 
 git clone https://github.com/arosenkranz/workshop-microsite.git /storedog-microsite
 cd /storedog-microsite
-docker build -f ./Dockerfile.dev . -t storedog-microsite
+npm install --silent
+npm i -g craco --silent
 
 # Fetch discounts in the browser
 mv /root/home_controller.rb /ecommworkshop/store-frontend-instrumented-fixed/app/controllers/spree/home_controller.rb
