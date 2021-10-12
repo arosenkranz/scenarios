8fb43d210b0cf4b82e643155648610c288dee0f7
Alex Rosenkranz <alex.rosenkranz@datadoghq.com>
2021-10-12 08:55:28 -0400

    fixes logs, hopefully

--- dd101-developer/04-logs/background.sh
+++ dd101-developer/04-logs/background.sh
@@ -2,14 +2,8 @@
 
 curl -sk https://datadoghq.dev/katacodalabtools/r?raw=true|bash
 
-statuscheck "environment"
 
-# Wait for required assets to appear in the filesystem
-until  [ -f /root/frontend-docker-entrypoint.sh ]
-do
-  sleep 2
-done
-
+echo 
 # Prep for Ecommerce Workshop
 git clone https://github.com/DataDog/ecommerce-workshop.git /ecommworkshop
 cd /ecommworkshop
@@ -20,8 +14,6 @@ cd /root
 
 mv /root/docker-compose-fixed.yml /ecommworkshop/deploy/docker-compose/
 mv /root/docker-compose-broken.yml /ecommworkshop/deploy/docker-compose/
-statusupdate "files"
+statusupdate files
 
 /ecommworkshop/gor --input-file-loop --input-file "/ecommworkshop/requests_0.gor|300%" --output-http "http://localhost:3000" >> /dev/null 2>&1
-
-statusupdate "workspace"
