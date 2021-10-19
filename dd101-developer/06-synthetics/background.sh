5e8836e97a005720628d820af38a6bdfaa50dad9
Alex Rosenkranz <alex.rosenkranz@datadoghq.com>
2021-10-19 19:45:41 -0400

    updates synthetics

--- dd101-developer/06-synthetics/background.sh
+++ dd101-developer/06-synthetics/background.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 curl -sk https://datadoghq.dev/katacodalabtools/r?raw=true|bash
 
+statuscheck "environment"
+
 until  [ -f /root/docker-compose.yml ]
 do
   sleep 2
 done
 
-statuscheck "environment"
-
+mv /root/docker-compose.yml /root/lab/docker-compose.yml
 ln -s /ecommworkshop/store-frontend-instrumented-fixed /root/lab/store-frontend
 ln -s /ecommworkshop/discounts-service /root/lab/discounts-service
 ln -s /ecommworkshop/ads-service /root/lab/ads-service
